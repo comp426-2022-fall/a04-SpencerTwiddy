04a1671570064ed832f540935324828436023566
Spencer Twiddy <sjt49@live.unc.edu>
2022-12-10 16:23:55 -0500

    type module

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const minimist = require('minimist');
+import { roll } from "./lib/roll.js"
 var options = minimist(process.argv.slice(2));
 const app = express();
 let port = 5000;
@@ -9,7 +10,10 @@ if (options.port !== undefined) {
 app.get('/app/', (req, res, next) => {
 	res.status(200).send('200 OK');
 });
-
+app.get('/app/roll/', (req, res, next) => {
+	let outp = JSON.stringify(roll(options.sides, options.dice, options.rolls));
+	res.send(outp);
+});
 app.get('*', function(req, res){
   res.status(404).send('404 NOT FOUND');
 });
