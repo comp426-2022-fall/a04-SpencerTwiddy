426c5007a8f999743dad25901729f3b184d936de
Spencer Twiddy <sjt49@live.unc.edu>
2022-12-10 16:09:52 -0500

    got home working

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('im fed');
+const express = require('express');
+const minimist = require('minimist');
+var options = minimist(process.argv.slice(2));
+const app = express();
+let port = 5000;
+if (options.port !== undefined) {
+	port = options.port;
+}
+app.get('/app/', (req, res, next) => {
+	res.status(200).send('200 OK');
+});
+
+app.listen(port, () => {});
